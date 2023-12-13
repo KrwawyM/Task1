f4ad7557f5befdb1f8b2b8b5cb4d7830de0a21e3
wikto <wikto@KOMPWIKI>
2023-12-13 13:39:39 +0100

    Add project files.

---
+++ Task1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            string name = "Wiktor";
+
+            string surname = "Włoszczyk";
+
+            int age = 33;
+
+            char sex = 'M';
+
+            long PESEL = 12121223234;
+
+            long ID = 2509324094;
+
+            Console.Write("Imie: ");
+            Console.WriteLine(name);
+
+            Console.Write("Nazwisko: ");
+            Console.WriteLine(surname);
+
+            Console.Write("Wiek: ");
+            Console.WriteLine(age);
+
+            Console.Write("Płeć: ");
+            Console.WriteLine(sex);
+
+            Console.Write("Pesel: ");
+            Console.WriteLine(PESEL);
+
+            Console.Write("Nr pracownika: ");
+            Console.WriteLine(ID);
+        }
+    }
+}
